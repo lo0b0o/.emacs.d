3417c70f8908e30f19f60e635e2803c44da8ed88
lo0b0o <tctsui@outlook.com>
2020-10-09 09:32:23 +0800

    add setting

---
+++ init.el
@@ -0,0 +1,81 @@
+(setq default-directory "d:/emacs-27.1")
+(require 'package)
+
+
+(setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
+                         ("melpa-stable" . "https://mirrors.ustc.edu.cn/elpa/melpa-stable/")
+                         ("org" . "https://mirrors.ustc.edu.cn/elpa/org/")))
+;;Auctex
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+;; cdlatex
+(add-hook 'LaTeX-mode-hook 'turn-on-cdlatex)   ; with AUCTeX LaTeX mode
+(add-hook 'latex-mode-hook 'turn-on-cdlatex)   ; with Emacs latex mode
+
+;; Use pdf-tools to open PDF files
+(setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+      TeX-source-correlate-start-server t)
+
+;; Update PDF buffers after successful LaTeX runs
+(add-hook 'TeX-after-compilation-finished-functions
+           #'TeX-revert-document-buffer)
+
+(add-hook 'LaTeX-mode-hook
+      (lambda()
+        (local-set-key [C-tab] 'TeX-complete-symbol)))
+
+(package-initialize)
+
+;;auto-complete
+(ac-config-default)
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; Activate auto-complete for latex modes (AUCTeX or Emacs' builtin one).
+(add-to-list 'ac-modes 'latex-mode)
+
+;; Activate ac-math.
+(eval-after-load "latex"
+  '(when (featurep 'auto-complete)
+     ;; See https://github.com/vspinu/ac-math
+     (require 'ac-math)
+     (defun ac-latex-mode-setup ()       ; add ac-sources to default ac-sources
+       (setq ac-sources
+         (append '(ac-source-math-unicode ac-source-math-latex ac-source-latex-commands)
+             ac-sources)))
+     (add-hook 'LaTeX-mode-hook 'ac-latex-mode-setup)))
+(ac-set-trigger-key "TAB")
+
+;;highlight latex in orgmode
+(eval-after-load 'org
+  '(setf org-highlight-latex-and-related '(latex)))
+
+
+(unless package--initialized (package-initialize t))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" default))
+ '(package-selected-packages
+   '(ac-math auto-complete-auctex auto-complete pdf-tools cdlatex auctex gruvbox-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(load-theme 'gruvbox-dark-medium t)
+(put 'upcase-region 'disabled nil)
